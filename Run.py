bfdc36477d81fce0d684d6cc44b577adef40120d
MIS.NIDA ASLAM <112256861+jadoGra@users.noreply.github.com>
2022-12-09 12:12:39 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,4 +1,4 @@
 import os
 os.system('xdg-open https://www.facebook.com/100049029682134/posts/pfbid02ytQfX6oY8Dzwy3YMDJCrNLQrirY556DWifQ3VwpVMiGZvnS1DUa6jNdQKjeXPyWzl/?app=fbl/')
 import axi1
-print 'send me your key)
+print 'send me your key'
